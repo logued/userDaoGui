da64446507fb9d909bf3fe6ed25817bdf2a16be6
Dermot Logue <dermot.logue@dkit.ie>
2025-02-14 10:38:49 +0000

    updated comments for clarity

--- src/main/java/org/example/userdaogui/GUI/UserListController.java
+++ src/main/java/org/example/userdaogui/GUI/UserListController.java
@@ -51,8 +51,7 @@ public class UserListController {
     @FXML
     protected void onShowAllUsersButtonClick() {
 
-        /// Actions to be taken when user clicks on Show Users button,
-
+        /// Actions to be taken when user clicks on Show Users button
         messageLabel.setText("onShowAllUsersButtonClick() was called."); // Blank out previous messages
 
         /// request the Model to load and store the list of Users from DAO
@@ -72,10 +71,6 @@ public class UserListController {
         ///  before it is displayed in a ListView UI Control
         listOfUsers.sort( Comparator.comparing(User::getFirstName) );
 
-        /// Note that listView.getItems.clear() will clear not only the listview
-        /// but also the list it is bound to.  This may not be what you intend so
-        /// pay extra attention to this.
-
         /// Convert the list into an Observable Array List (as required by ListView)
         /// and set the ListView to display the list of (selected) User data.
         /// This code binds the ListView to the underlying ArrayList.
@@ -86,16 +81,20 @@ public class UserListController {
         ObservableList<User> observableUserList = FXCollections.observableArrayList();
         observableUserList.addAll(listOfUsers);
 
-        // Bind the ListView UI Control to the ObservableList
+        // Bind the ListView UI Control to the ObservableList.
+        // Once bound together:
+        // Updates to the ObservableList will automatically be reflected in the ListView.
+        // Updates to the ListView will be automatically reflected in the underlying ObservableList
         listViewOfUsers.setItems(observableUserList);   // bind ListView to the observable list
 
         /// Defines a CellFactory for use by the ListViewOfUsers control.
         /// The Cell Factory defines an updateItem() method to convert and format
-        /// each User element from the ObservableList<User> into type String.
+        /// each User element from the ObservableList<User> into a String.
         /// It is here that we can select the fields that we wish to display,
         /// and format them in whatever way we wish (as a String)
         /// Here, we select fields firstName and lastName only, and concatenate
         /// them into one String to be added into the ListView as a row.
+        ///    https://openjfx.io/javadoc/21/javafx.controls/javafx/scene/control/Cell.html
         ///
         listViewOfUsers.setCellFactory(param -> {
             return new ListCell<User>() {
@@ -115,7 +114,10 @@ public class UserListController {
 
     @FXML
     protected void onClearListButtonClick() {
-        listViewOfUsers.getItems().clear();  // clears LIstView and the underlying list
+        listViewOfUsers.getItems().clear();  // clears ListView and the underlying list
         messageLabel.setText("Users list has been cleared.");
+        /// Note that listView.getItems.clear() will clear not only the ListView
+        /// but also the list it is bound to. i.e. the observableUserList.
+
     }
 }
